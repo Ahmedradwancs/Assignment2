97ed74f8e053d311fd8a7b95ea4facd3029f082c
Ahmed Radwan <ahmed_saber_elsayed.radwan0004@stud.hkr.se>
2024-02-06 19:24:37 +0100

    Update StringFunctions.cs

--- Assignment2/StringFunctions.cs
+++ Assignment2/StringFunctions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,10 @@ namespace Assignment2
 {
     internal class StringFunctions
     {
-        // Calculate the length of a string and return the length
+        /// <summary>
+        /// When the user selects the StringFunctions option from the main menu, this method is called.
+        /// </summary>
+        /// <param name="input"></param>
         public void StringLength(string input)
         {
             int length = input.Length;
@@ -19,45 +22,47 @@ namespace Assignment2
             Console.WriteLine();
         }
 
-        // Create PredictMyDay method
+        /// <summary>
+        /// This method predicts the user's day based on the number he/she selects.
+        /// </summary>
         public void PredictMyDay()
         {
             Console.WriteLine();
             Console.WriteLine("********** The Fortune Teller **********");
             Console.WriteLine("Let me predict your day! Select a number between 1 and 7: ");
-            int day = int.Parse(Console.ReadLine());
+            String day = Console.ReadLine();
             switch (day)
             {
-                case 1:
+                case "1":
                     Console.WriteLine("Keep calm on Mondays! You can fall apart!");
                     break;
 
-                case 2:
+                case "2":
                     Console.WriteLine("Tuesdays and Wednesdays break your heart!");
                     break;
 
-                case 3:
+                case "3":
                     Console.WriteLine("Tuesdays and Wednesdays break your heart!");
                     break;
 
-                case 4:
+                case "4":
                     Console.WriteLine("Thursday is your lucky day, don't wait for Friday!");
                     break;
 
-                case 5:
+                case "5":
                     Console.WriteLine("Friday, you are in love!");
                     break;
 
-                case 6:
+                case "6":
                     Console.WriteLine("Saturday, do nothing and do plenty of it!");
                     break;
 
-                case 7:
+                case "7":
                     Console.WriteLine("And Sunday always comes too soon!");
                     break;
 
                 default:
-                    Console.WriteLine("No day? is a good day but it doesn't exist!");
+                    Console.WriteLine("No day? A good day but it doesn't exist!");
                     break;
             }
             Console.WriteLine();
@@ -65,10 +70,12 @@ namespace Assignment2
 
         }
 
-
+        /// <summary>
+        /// This method asks the user if he/she wants to continue with another round.
+        /// </summary>
         public void RunAgain()
         {
-            Console.WriteLine("Continue with another round? (y/n)");
+            Console.Write("Continue with another round? (y/n): ");
             string answer = Console.ReadLine();
             if (answer.ToLower() == "y")
             {
@@ -85,7 +92,10 @@ namespace Assignment2
             }
 
         }
-        // Create a method that starts the program
+
+        /// <summary>
+        /// This method starts the program.
+        /// </summary>
         public void Start()
         {
                 // Console.WriteLine("++++++++++++++++ String Functions!  ++++++++++++");
